d42362127924a7eac9fbc62e004426d2cb958507
Joshua Chen <joshua6261@att.net>
2024-11-08 01:08:06 -0800

    Make horizontal split bar visible

--- lua/options.lua
+++ lua/options.lua
@@ -63,3 +63,5 @@ vim.opt.scrolloff = 10
 vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'gray', bold = false })
 vim.api.nvim_set_hl(0, 'LineNr', { fg = '#ffe5b4', bold = true })
 vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'gray', bold = false })
+
+vim.opt.laststatus = 3
