fa5538925f9bd050cd8488c627334c5b4edd6099
Joshua Chen <joshua.chen@toyon.com>
2024-12-26 17:59:03 -0800

    Modify toggleterm config

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -2,16 +2,18 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   opts = {
+    size = function(term)
+      if term.direction == 'horizontal' then
+        return 30
+      elseif term.direction == 'vertical' then
+        return vim.o.columns * 0.4
+      end
+    end,
     open_mapping = [[<leader>tt]],
-    direction = 'float',
-    float_opts = {
-      border = 'curved',
-      title_pos = 'center',
-      height = 50,
-    },
+    direction = 'horizontal',
     start_in_insert = true,
     close_on_exit = true,
-    insert_mappings = true,
+    insert_mappings = false,
     terminal_mappings = true,
   },
 }
